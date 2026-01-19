07982af37dd480902f36cb4d47427e3c29afed23
Adeyemi Pelumi <adeyemipelumi1980@gmail.com>
2026-01-19 14:10:24 +0100

    update on the code

--- src/page/Welcome.jsx
+++ src/page/Welcome.jsx
@@ -12,19 +12,19 @@ const Welcome = () => {
 
         <div>
           <p
-            className="text-[12px] sm:text-[14px] md:text-[16px] lg:text-[18px] font-bold text-center mt-6"
+            className="text-[12px] sm:text-[14px] md:text-[16px] lg:text-[18px] font-bold text-center pt-40"
             text="BUY, SELL & SWAP CRYPTO op"
           >
             Log in as
           </p>
         </div>
-        <Link to="/buy-crypto" className="flex items-center">
-          <button className="text-[12px] sm:text-[14px] md:text-[16px] lg:text-[18px] bg-[#0063BF] w-full sm:w-[292px] h-[54px] text-white mx-auto rounded-3xl cursor-pointer hover:animate-pulse font-bold op mt-6 ">
+        <Link to="/buy-crypto" className="flex items-center pt-30 mb-4">
+          <button className="text-[12px] sm:text-[14px] md:text-[16px] lg:text-[18px] bg-[#0063BF] w-full sm:w-[350px] h-[54px] text-white mx-auto rounded-3xl cursor-pointer hover:animate-pulse font-bold op mt-6 ">
             User
           </button>
         </Link>
         <Link className="flex items-center" to="/admin-login">
-          <button className="text-[12px] sm:text-[14px] md:text-[16px] lg:text-[18px] bg-[#E3EAF2] w-full sm:w-[292px] h-[54px] text-[#0063BF] mx-auto rounded-3xl font-bold op mt-3 cursor-pointer hover:animate-pulse">
+          <button className="text-[12px] sm:text-[14px] md:text-[16px] lg:text-[18px] bg-[#E3EAF2] w-full sm:w-[350px] h-[54px] text-[#0063BF] mx-auto rounded-3xl font-bold op mt-3 cursor-pointer hover:animate-pulse">
             Admin
           </button>
         </Link>
